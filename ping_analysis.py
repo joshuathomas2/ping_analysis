25c5e48b0791a9bde554ec89d06aac74dccdabe1
Josh Thomas <joshuathomas2@mail.weber.edu>
2019-06-03 13:57:59 -0600

    added error example file, changed some whitespace, no lines go over col 80

--- ping_analysis.py
+++ ping_analysis.py
@@ -53,7 +53,8 @@ def analyze_file(file_name):
     for line in text_file:
         if (line[0:5] == "Reply"):
             ping_count += 1
-            ping_list.append(int(re.split("[ ]", line)[4][5:][:-2]))
+            ping_time = int(re.split("[ ]", line)[4][5:][:-2])
+            ping_list.append(ping_time)
 
     if (len(ping_list) == 0):
         print_error("no_data")
@@ -76,19 +77,29 @@ def analyze_file(file_name):
     print("====================================================")
     print("[", file_name, "]", "Total ping count: ", ping_count)
     print("====================================================")
-    print("Tiny ping count:", tiny_ping_count, "   (>1ms)", round((tiny_ping_count / ping_count) * 100,2), "%")
-    print("Small ping count:", small_ping_count, " (>25ms)", round((small_ping_count / ping_count) * 100,2), "%")
-    print("Medium ping count:", medium_ping_count, "  (>75ms)", round((medium_ping_count / ping_count) * 100,2), "%")
-    print("Large ping count:", large_ping_count, "  (>100ms)", round((large_ping_count / ping_count) * 100,2), "%")
-    print("Extreme ping count:", extreme_ping_count, "(>200ms)", round((extreme_ping_count / ping_count) * 100,2), "%")
+    
+    print("Tiny ping count:", tiny_ping_count, "   (>1ms)",
+    round((tiny_ping_count / ping_count) * 100,2), "%")
+    
+    print("Small ping count:", small_ping_count, " (>25ms)",
+    round((small_ping_count / ping_count) * 100,2), "%")
+    
+    print("Medium ping count:", medium_ping_count, "  (>75ms)",
+    round((medium_ping_count / ping_count) * 100,2), "%")
+    
+    print("Large ping count:", large_ping_count, "  (>100ms)",
+    round((large_ping_count / ping_count) * 100,2), "%")
+    
+    print("Extreme ping count:", extreme_ping_count, "(>200ms)",
+    round((extreme_ping_count / ping_count) * 100,2), "%")
+    
     print_space(1)
     print("MAXIMUM ping:", max(ping_list))
     print("MINIMUM ping:", min(ping_list))
     print("AVERAGE ping:", average_ping)
     print("====================================================")
-
-    print_space(3)
-
+    print_space(1)
+    
     return 1
 
 def print_error(error):
