92e02f1d5d24140a4b15069338215f70b18b9c45
Josh Thomas <joshuathomas2@mail.weber.edu>
2019-06-12 23:49:11 -0600

    removed old pinganalysis.py and renamed pinganalysisgui.py to pinganalysis.py

--- pinganalysis.py
+++ pinganalysis.py
@@ -1,137 +1,191 @@
+from tkinter import *
+from tkinter import filedialog
 import os
 import re
-import pinganalysisgui as pag
+
+
+class PingAnalysisGui:
+    def __init__(self):
+        self.DIRECTORY = "raw_data"
+        self.FONT = ("Times New Roman", "12")
+        self.FONT_LARGE = ("Times New Roman", "24", "bold")
+        self.FONT_MEDIUM = ("Times New Roman", "16", "bold")
+
+        self.root = Tk()
+        self.root.title("Ping Analysis")
+        self.root.geometry("750x500")
+
+        self.main_frame = Frame(self.root)
+        self.main_frame.pack(side=LEFT, fill=BOTH)
+
+        self.data_frame = Frame(self.root)
+        self.data_frame.pack(side=RIGHT, fill=BOTH, expand=1)
+
+        self.listbox_data = Listbox(self.main_frame, bg="gray", height=25)
+        self.listbox_data.pack(side=TOP)
+
+        self.label_title = Label(self.data_frame, height=3, text="Ping Analysis", font=self.FONT_LARGE)
+        self.label_title.pack(fill=X)
+
+        self.label_error = Label(self.data_frame, fg="red", font=self.FONT_LARGE)
+        self.label_error.pack()
+
+        self.label_file = Label(self.data_frame, height=2, font=self.FONT_MEDIUM)
+        self.label_file.pack()
+
+        self.label_tiny_ping = Label(self.data_frame)
+        self.label_tiny_ping.pack()
+
+        self.label_small_ping = Label(self.data_frame)
+        self.label_small_ping.pack()
+
+        self.label_medium_ping = Label(self.data_frame)
+        self.label_medium_ping.pack()
+
+        self.label_large_ping = Label(self.data_frame)
+        self.label_large_ping.pack()
+
+        self.label_extreme_ping = Label(self.data_frame)
+        self.label_extreme_ping.pack()
+
+        self.label_max_ping = Label(self.data_frame, font=self.FONT_MEDIUM)
+        self.label_max_ping.pack()
+
+        self.label_min_ping = Label(self.data_frame, font=self.FONT_MEDIUM)
+        self.label_min_ping.pack()
+
+        self.label_mean_ping = Label(self.data_frame, font=self.FONT_MEDIUM)
+        self.label_mean_ping.pack()
+
+        self.label_lag_count = Label(self.data_frame, font=self.FONT_MEDIUM)
+        self.label_lag_count.pack()
+
+        self.button_analyze = Button(self.main_frame, text="Analyze")
+        self.button_analyze.pack(fill=BOTH, expand=1)
+
+        self.button_refresh = Button(self.main_frame, text="Refresh List")
+        self.button_refresh.pack(fill=BOTH, expand=1)
+
+        self.button_open_folder = Button(self.main_frame, text="Open Folder")
+        self.button_open_folder.pack(fill=BOTH, expand=1)
+
+        self.file_dialog = filedialog
+
+        self.selection_index = None
+        self.selection = None
+
+    def start_mainloop(self):
+        self.root.mainloop()
+
+    def populate_listbox(self, data):
+        self.clear_listbox()
+        for item in data:
+            self.listbox_data.insert("end", item)
+
+    def clear_listbox(self):
+        self.listbox_data.delete(0, END)
+
+    def get_data(self, directory):
+        data = os.listdir(directory)
+        return data
+
+    def get_selection(self):
+        self.selection_index = self.listbox_data.curselection()
+
+        try:
+            self.selection = self.listbox_data.get(self.selection_index)
+            print(self.selection)
+        except TclError:
+            print("Invalid selection")
+
+    def open_folder(self):
+        self.file_dialog.askopenfilename(initialdir=f"{self.DIRECTORY}")
+
+    def configure_buttons(self):
+        self.button_analyze.configure(command=self.analyze)
+        self.button_refresh.configure(command=lambda: self.populate_listbox(self.get_data(self.DIRECTORY)))
+        self.button_open_folder.configure(command=self.open_folder)
+
+    def clear_labels(self):
+        self.label_tiny_ping.configure(text="")
+        self.label_small_ping.configure(text="")
+        self.label_medium_ping.configure(text="")
+        self.label_large_ping.configure(text="")
+        self.label_extreme_ping.configure(text="")
+        self.label_max_ping.configure(text="")
+        self.label_min_ping.configure(text="")
+        self.label_mean_ping.configure(text="")
+        self.label_file.configure(text="")
+        self.label_lag_count.configure(text="")
+
+    def analyze(self):
+        ping_list = []
+        ping_count = 0
+        lag_count = 0
+        mean_ping = 0
+        tiny_ping_count = 0
+        small_ping_count = 0
+        medium_ping_count = 0
+        large_ping_count = 0
+        extreme_ping_count = 0
+        error = False
+
+        self.clear_labels()
+        self.label_error.configure(text="")
+        self.get_selection()
+
+        if self.selection is None:
+            self.label_error.configure(text="ERROR: No file selected")
+            error = True
+        else:
+            raw_data_file = open(self.DIRECTORY + "/" + self.selection)
+
+            for line in raw_data_file:
+                if line[0:5] == "Reply":
+                    ping_count += 1
+                    ping_time = int(re.split(" ", line)[4][5:][:-2])
+                    ping_list.append(ping_time)
+
+            if len(ping_list) == 0:
+                self.label_error.configure(text="ERROR: No data or file is not UTF-8")
+                error = True
+            else:
+                mean_ping = int((sum(ping_list) / len(ping_list)))
+
+        if error:
+            pass
+        else:
+            for ping in ping_list:
+                if ping > 200:
+                    extreme_ping_count += 1
+                elif ping > 100:
+                    large_ping_count += 1
+                elif ping > 75:
+                    medium_ping_count += 1
+                elif ping > 25:
+                    small_ping_count += 1
+                elif ping > 1:
+                    tiny_ping_count += 1
+
+            lag_count = medium_ping_count + large_ping_count + extreme_ping_count
+
+            self.label_file.configure(text=f"[{self.selection}] Total ping count: {ping_count}")
+            self.label_tiny_ping.configure(text=f"Tiny ping count: {tiny_ping_count} (>1ms)")
+            self.label_small_ping.configure(text=f"Small ping count: {small_ping_count} (>25ms)")
+            self.label_medium_ping.configure(text=f"Medium ping count: {medium_ping_count} (>75ms)")
+            self.label_large_ping.configure(text=f"Large ping count: {large_ping_count} (>100ms)")
+            self.label_extreme_ping.configure(text=f"Extreme ping count: {extreme_ping_count} (>200ms)")
+            self.label_max_ping.configure(text=f"MAXIMUM ping count: {max(ping_list)}")
+            self.label_min_ping.configure(text=f"MINIMUM ping count: {min(ping_list)}")
+            self.label_mean_ping.configure(text=f"MEAN ping count: {mean_ping}")
+            self.label_lag_count.configure(text=f"Lagged {lag_count} times out of {ping_count} ({round(lag_count / ping_count * 100, 2)}%)")
 
 
 def main():
-
-
-
-
-
-    """
-    #user_choices = list(zip(file_count, data))
-
-    for choice in user_choices:
-        print(choice[0], ":", choice[1])
-
-    print_space(1)
-        
-    choice = input("Enter the number of the file you wish to analyze (enter nothing to quit): ")
-
-    if (choice == ""):
-        sys.exit()
-
-    try:
-        choice = int(choice)
-    except ValueError:
-        print_error("invalid_number")
-        main()
-
-    if ((choice > len(user_choices) - 1) or (choice < 0)):
-        print_error("invalid_number")
-        main()
-    else:
-        choice = user_choices[choice][1]
-            
-    return choice
-    """
-
-
-def analyze_file(file_name):
-    ping_list = []
-    ping_count = 0
-    lagspike_count = 0
-    average_ping = 0
-    tiny_ping_count = 0
-    small_ping_count = 0
-    medium_ping_count = 0
-    large_ping_count = 0
-    extreme_ping_count = 0
-
-    print_space(14)
-    print("Attempting to analyze... " + file_name)
-
-    text_file = open("data/" + file_name, "r")
-        
-    for line in text_file:
-        if line[0:5] == "Reply":
-            ping_count += 1
-            ping_time = int(re.split(" ", line)[4][5:][:-2])
-            ping_list.append(ping_time)
-
-    if len(ping_list) == 0:
-        print_error("no_data")
-        main()
-    else:
-        average_ping = int((sum(ping_list)/len(ping_list)))
-    
-    for ping in ping_list:
-        if ping > 20:
-            extreme_ping_count += 1
-        elif ping > 100:
-            large_ping_count += 1
-        elif ping > 75:
-            medium_ping_count += 1
-        elif ping > 25:
-            small_ping_count += 1
-        elif ping > 1:
-            tiny_ping_count += 1
-
-    lagspike_count = medium_ping_count + large_ping_count + extreme_ping_count
-
-
-
-    """
-    print("====================================================")
-    print("[", file_name, "]", "Total ping count: ", ping_count)
-    print("====================================================")
-    
-    print("Tiny ping count:", tiny_ping_count, "   (>1ms)",
-    round((tiny_ping_count / ping_count) * 100,2), "%")
-    
-    print("Small ping count:", small_ping_count, " (>25ms)",
-    round((small_ping_count / ping_count) * 100,2), "%")
-    
-    print("Medium ping count:", medium_ping_count, "  (>75ms)",
-    round((medium_ping_count / ping_count) * 100,2), "%")
-    
-    print("Large ping count:", large_ping_count, "  (>100ms)",
-    round((large_ping_count / ping_count) * 100,2), "%")
-    
-    print("Extreme ping count:", extreme_ping_count, "(>200ms)",
-    round((extreme_ping_count / ping_count) * 100,2), "%")
-    
-    print_space(1)
-    print("MAXIMUM ping:", max(ping_list))
-    print("MINIMUM ping:", min(ping_list))
-    print("MEAN ping:", average_ping)
-    print_space(1)
-    print("Lagged", lagspike_count, "times out of", ping_count, "(>75ms)",
-    round((lagspike_count / ping_count) * 100,2), "%")
-    print("====================================================")
-    print_space(13)
-    """
-    input("Press enter to continue...")
-    print_space(20)
-    
-    return 1
-
-
-def print_error(error):
-    if error == "no_data":
-        print_space(1)
-        print("Error with text file: No data found or file is not in UTF-8")
-        print_space(1)
-    elif error == "invalid_number":
-        print_space(1)
-        print("Invalid number")
-        print_space(1)
-
-
-def print_space(number):
-    for i in range(number):
-        print("")
+    pag = PingAnalysisGui()
+    pag.configure_buttons()
+    pag.populate_listbox(pag.get_data(pag.DIRECTORY))
+    pag.start_mainloop()
 
 
 if __name__ == "__main__":
